c490697a8aede6e46bd7a0c10f90668b51fe853e
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-16 12:28:53 +0300

    2 files modified, 1 added

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ const person = {
 // Way to output object items
 // console.log(person['name'])
 // console.log(person.name)
+
+person.nickname = "Duke"
+
+const message = `Hi, Im ${person.name}. I live in ${person.city}. Most know me as ${person.nickname}. I have ${person.skills.length} skills`
+console.log(message)
+
+
+// person.name = "John"
+// console.log(person.name)
