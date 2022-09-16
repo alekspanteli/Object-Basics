e53c0fce544313efb1a418a316defb9cb253734c
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-16 12:56:50 +0300

    2 files modified, 1 added

---
+++ looping-objects.js
@@ -0,0 +1,47 @@
+
+// const person = {
+//   name: "Alex",
+//   city: "Ayia Napa",
+//   age: 20,
+//   isStudent: true,
+//   skills: ["HTML", "CSS", "JS"]
+// }
+
+// const doc = document.querySelector('main');
+
+// for (let prop in person){
+//   doc.innerHTML += `<li>${person[prop]}</li>`;
+// }
+
+
+// const person = {
+//   name: 'Reggie',
+//   role: 'Software developer',
+//   skills: ['JavaScript', 'HTML', 'CSS'],
+//   isTeacher: true
+// };
+
+
+// Store the keys of the `person` object in `personProps`
+// const personProps = Object.keys(person); 
+// const personVals = Object.values(person); 
+// console.log(personVals);
+
+
+// const name = {
+//   firstName: 'Reggie',
+//   lastName: 'Williams',
+// };
+
+// const role = {
+//   title: 'Software developer',
+//   skills: ['JavaScript', 'HTML', 'CSS'],
+//   isTeacher: true
+// };
+
+// // merge `name` and `role` into a `person` object
+// const person = {  
+//   ...name,
+//   ...role
+// };
+// console.log(person);
